21aac96e8eb747ee1850fdaa8589e7d3f914d286
Unknown <vk1998@mail.ru>
2017-12-11 23:58:39 +0300

    Начата осомысленная работа над уровнем

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "widget.h"
+#include<iostream>
+#include <stdio.h>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    std::ios::sync_with_stdio(false);
     QApplication a(argc, argv);
     Widget w;
     w.show();
